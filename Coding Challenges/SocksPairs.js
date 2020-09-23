49559a96da36dda59161cf673212b6965d8d2875
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-23 21:57:19 +0200

    update 17

--- Coding Challenges/SocksPairs.js
+++ Coding Challenges/SocksPairs.js
@@ -1,15 +1,6 @@
 
 // Socks Pairs Coding Challenge - Hacker Rank
-
-// Alex works at a clothing store. There is a large pile of socks that must be paired by color for sale. 
-//Given an array of integers representing the color of each sock, 
-// determine how many pairs of socks with matching colors there are.
-
-// For example, there are  socks with colors . There is one pair of color  and one of color . 
-// There are three odd socks left, one of each color. The number of pairs is .
-
-
-// let arr = [1, 2, 1, 2, 1, 3, 2, 3];
+// https://www.hackerrank.com/challenges/sock-merchant/problem
 
 let arr = [10, 20, 20, 10, 10, 30, 50, 10, 20];
 
