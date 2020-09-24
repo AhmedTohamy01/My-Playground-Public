83cc2890828501682dd995becdb447c26137f77d
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-24 21:37:45 +0200

    update 1

--- Coding Challenges/test.js
+++ Coding Challenges/test.js
@@ -1,20 +1,23 @@
 // let q = [2,1,5,3,4];
-let q = [1, 2, 5, 3, 7, 8, 6, 4]
-let q = [1, 2, 5, 3, 7, 8, 6, 4]
-
-1 2 5 3 4 7 8 6
+// let q = [1, 2, 5, 3, 7, 8, 6, 4]
+let q = [1, 2, 5, 3, 4, 7, 8, 6];
 
 
 function test() {
+
   let counter = 0;
   for (let i = 0; i < q.length; i++) {
     if (q[i] > q[i+1]) {
-      let currentItem = q[i]
-      q[i] = q[i+1];
-      q[i+1] = currentItem;
-      counter ++;
-      i = 0;
-      console.log(q);
+      if (q[i]) {
+
+      } else {
+        let currentItem = q[i]
+        q[i] = q[i+1];
+        q[i+1] = currentItem;
+        counter ++;
+        i = 0;
+        // console.log(q);
+      }
     }
   }
   return counter;
