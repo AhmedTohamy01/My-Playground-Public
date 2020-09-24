20b70f5d70c8de0ebb68b85e86413f2018b97ee8
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-24 20:37:55 +0200

    updat 10

--- Coding Challenges/test.js
+++ Coding Challenges/test.js
@@ -1,21 +1,20 @@
-// let q = [2,5,1,3,4];
-let q = [2,1,5,3,4];
+// let q = [2,1,5,3,4];
+let q = [1, 2, 5, 3, 7, 8, 6, 4]
+
 
 
 function test() {
   let counter = 0;
-    if (q[0] < q[1]) {
-      console.log('Too chaotic');
-    } else if (q[0] > q[1]) {
-      for (let i = 0; i < q.length; i++) {
-        if (q[i] > q[i+1]) {
-          let currentItem = q[i]
-          q[i] = q[i+1];
-          q[i+1] = currentItem;
-          counter ++;
-      }
-    } 
-    console.log(counter);
+  for (let i = 0; i < 8; i++) {
+    if (q[i] > q[i+1]) {
+      let currentItem = q[i]
+      q[i] = q[i+1];
+      q[i+1] = currentItem;
+      counter ++;
+      console.log(q);
     }
+  }
+  return counter;
 }
+
 console.log(test());
