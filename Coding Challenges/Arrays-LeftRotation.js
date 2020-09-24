db011ee257ece7583ad87f2a4635aa444108ea11
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-24 19:51:05 +0200

    Add more test

--- Coding Challenges/Arrays-LeftRotation.js
+++ Coding Challenges/Arrays-LeftRotation.js
@@ -2,8 +2,14 @@
 // https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
 
 
-let arr = [1,2,3,4,5];
-let rotation = 4;
+// let arr = [1,2,3,4,5];
+
+let arr = [41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51];
+
+let arr = [33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60, 87, 97];
+
+
+let rotation = 10;
 
 function test() {
   for (let i = rotation; i > 0; i--) {
