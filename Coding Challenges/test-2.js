456e6c7a3477f577b35928d621a0c1b124e268dc
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-24 23:14:24 +0200

    update 11

---
+++ Coding Challenges/test-2.js
@@ -0,0 +1,7 @@
+let q = [1, 2, 5, 3, 4, 7, 8, 6];
+
+let sortQ = q.sort();
+
+console.log(q.indexOf(1));
+
+console.log(sortQ)
--- Coding Challenges/test.js
+++ Coding Challenges/test.js
@@ -1,29 +1,31 @@
 // let q = [2,1,5,3,4];
 // let q = [1, 2, 5, 3, 7, 8, 6, 4]
-let q = [1, 2, 5, 3, 4, 7, 8, 6];
+// let q = [1, 2, 5, 3, 4, 7, 8, 6];
+// bad
+// let q = [5, 1, 2, 3, 7, 8, 6, 4];
+// let q = [2, 5, 1, 3, 4]
 
 
 function test() {
-
+  let sortQ = [...q].sort();
   let counter = 0;
   for (let i = 0; i < q.length; i++) {
     if (q[i] > q[i+1]) {
-      if (q[i]) {
-
-      } else {
         let currentItem = q[i]
         q[i] = q[i+1];
         q[i+1] = currentItem;
         counter ++;
         i = 0;
-        // console.log(q);
       }
-    }
   }
-  return counter;
+  console.log(counter);
 }
 
-console.log(test());
+test();
 
 
+// if ((sortQ.indexOf(q[i]) - i) > 2) {
+//   console.log('Too chaotic');
+//   return 'Too chaotic';
+// } 
 
