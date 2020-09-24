ac1a05c9f7a4511b5a1a3d40f4639c322d07f231
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-25 00:09:18 +0200

    Add New Year Chaos

--- Coding Challenges/NewYearChaos.js
+++ Coding Challenges/NewYearChaos.js
@@ -2,7 +2,7 @@
 // let q = [1, 2, 5, 3, 7, 8, 6, 4]
 // let q = [1, 2, 5, 3, 4, 7, 8, 6];
 // bad
-// let q = [5, 1, 2, 3, 7, 8, 6, 4];
+let q = [5, 1, 2, 3, 7, 8, 6, 4];
 // let q = [2, 5, 1, 3, 4]
 
 
@@ -16,12 +16,20 @@ function test() {
         q[i+1] = currentItem;
         counter ++;
         i = 0;
+        let oldLabel = sortQ.indexOf(currentItem);
+        let newLabel = q.indexOf(currentItem)-1;
+        // console.log(oldLabel);
+        // console.log(newLabel);
+        if ((oldLabel - newLabel) > 2) {
+          i = 100;
+          counter = 'Too chaotic';
+        }
       }
   }
-  console.log(counter);
+  return counter;
 }
 
-test();
+console.log(test());
 
 
 // if ((sortQ.indexOf(q[i]) - i) > 2) {
