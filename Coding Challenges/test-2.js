c8592e90e91f0f13aeb79f42da9246755ac3e633
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-25 18:32:11 +0200

    update 1

--- Coding Challenges/test-2.js
+++ Coding Challenges/test-2.js
@@ -3,3 +3,18 @@ let q = [1, 2, 5, 3, 4, 7, 8, 6];
 let sortQ = q.sort();
 
 
+let arr = [7, 1, 3, 2, 4, 5, 6];
+
+let sortArr = [...arr].sort();
+
+let min = Math.min(...arr);
+
+let newArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+  
+}
+
+
+
+console.log(min);
---
+++ Coding Challenges/test.js
@@ -0,0 +1,16 @@
+let arr = [7, 1, 3, 2, 4, 5, 6];
+
+let sortArr = [...arr].sort();
+
+let min = Math.min(...arr);
+
+let newArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+  
+}
+
+
+
+console.log(min);
+
