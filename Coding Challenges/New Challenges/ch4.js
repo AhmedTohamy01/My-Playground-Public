438637d2a4cc947bad1485d22930bd4fd747300b
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-09-26 22:52:20 +0200

    update 7

---
+++ Coding Challenges/New Challenges/ch4.js
@@ -0,0 +1,14 @@
+function test() {
+  let jumps = 0;
+  for (let i = 0; i < clouds.length; i++) {
+    if (clouds[i+2] === 0) {
+      jumps++;
+      i += 1;
+      // console.log(jumps)
+    } else if (clouds[i+1] === 0) {
+      jumps++;
+      // console.log(jumps)
+    }
+  }
+  return jumps;
+}
