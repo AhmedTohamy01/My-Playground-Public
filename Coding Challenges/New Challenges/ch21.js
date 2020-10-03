bce7d5eb1c2aaeadcfc4d15f3d234990e6357919
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-03 19:19:36 +0200

    Remove Challenge 21

--- Coding Challenges/New Challenges/ch21.js
+++
@@ -1,9 +0,0 @@
-var map = require("./"); // <- this is the file you make;
-
-var numbers = [1, 2, 3];
-
-var doubles = map(numbers, function (number) {
-  return number * 2;
-});
-
-console.log(doubles); // [2, 4, 6]
