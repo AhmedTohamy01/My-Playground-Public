7d2d89ca4c4673be9ca4ab97fa4a6e322cc354d5
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-08 17:42:55 +0200

    Update to Coding Challenge 25

--- Coding Challenges/New Challenges/ch25.js
+++
@@ -1,9 +0,0 @@
-var nextValue = 0;
-var asyncFactory = function () {
-  var resolveWith = nextValue++;
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve(resolveWith + "!");
-    }, Math.random() * 100);
-  });
-};
