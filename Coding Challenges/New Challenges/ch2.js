95151207b4f862f327d323e512d352266a39a1b1
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-09 12:54:05 +0200

    Update Coding Challenges

---
+++ Coding Challenges/New Challenges/ch2.js
@@ -0,0 +1,24 @@
+
+let arr = [
+  [-9,-9,-9,1,1,1],
+  [0,-9,0,4,3,2],
+  [-9,-9,-9,1,2,3],
+  [0,0,8,6,6,0],
+  [0,0,0,-2,0,0],
+  [0,0,1,2,4,0],
+]
+
+function hourGlass() {
+  let finalArr = [];
+  for (let x = 0; x < arr.length-2; x++) {
+    for (let y = 0; y < arr.length-2; y++) {
+      let sum = arr[x][y] + arr[x][y+1]+ arr[x][y+2]+ arr[x+1][y+1]+ arr[x+2][y]+ arr[x+2][y+1]+ arr[x+2][y+2]; 
+      finalArr.push(sum);
+    }
+  }
+  let max = Math.max(...finalArr);
+  return max;
+}
+
+
+console.log(hourGlass());
