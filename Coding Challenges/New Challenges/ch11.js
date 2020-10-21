b382f3d99a8f46c0550ad73bdcfe63b52ab9eb3a
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-10-22 00:12:35 +0200

    update ch11

--- Coding Challenges/New Challenges/ch11.js
+++ Coding Challenges/New Challenges/ch11.js
@@ -33,4 +33,3 @@ console.log(hourGlass());
 
 // Pseudo code:
 // - We should create a for loop to loop over the 2 layers of the array and get the item which can build
-//   the "glasshour" diagram.
