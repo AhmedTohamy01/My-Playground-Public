7771fa8729db3ce63a79c5204c5ddf5e19b78218
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-11 20:45:57 +0200

    update ch26

--- Coding Challenges/New Challenges/ch26.js
+++ Coding Challenges/New Challenges/ch26.js
@@ -1,40 +1,15 @@
-// 2D Array-DS - HackerRank
-// https://www.hackerrank.com/challenges/2d-array/problem
-
-let arr = [
-  [-9, -9, -9, 1, 1, 1],
-  [0, -9, 0, 4, 3, 2],
-  [-9, -9, -9, 1, 2, 3],
-  [0, 0, 8, 6, 6, 0],
-  [0, 0, 0, -2, 0, 0],
-  [0, 0, 1, 2, 4, 0],
-]
-
-function hourGlass() {
-  let finalArr = []
-  for (let x = 0; x < arr.length - 2; x++) {
-    for (let y = 0; y < arr.length - 2; y++) {
-      let sum =
-        arr[x][y] +
-        arr[x][y + 1] +
-        arr[x][y + 2] +
-        arr[x + 1][y + 1] +
-        arr[x + 2][y] +
-        arr[x + 2][y + 1] +
-        arr[x + 2][y + 2]
-      finalArr.push(sum)
+function twoNumberSum(array, targetSum) {
+  let newArr = []
+  for (let i = 0; i < array.length; i++) {
+    let reqNum = targetSum - array[i]
+    // console.log(reqNum);
+    if (newArr.includes(reqNum)) {
+      //  console.log(newArr.includes(reqNum));
+      return [reqNum, array[i]]
+    } else {
+      newArr.push(array[i])
     }
   }
-  let max = Math.max(...finalArr)
-  return max
+  return []
 }
-
-console.log(hourGlass())
-
-// Pseudo code:
-// - We should create a for loop to loop over the 2 layers of the array and get the item which can build
-//   the "glasshour" diagram.
-// - then we get the sum of these items.
-// - then we create an empty array, and push all the sums of each loop in this array.
-// - then we use Math.max to get the maximum number in the array.
-// ..........................
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
